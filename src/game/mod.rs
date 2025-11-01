d2fdfe0f4e904ddcf38f373658555ce3fd3bdb68
Raymond Bian <raybbian@gmail.com>
2025-10-31 23:48:17 -0400

    no debug

--- src/game/mod.rs
+++ src/game/mod.rs
@@ -49,7 +49,7 @@ impl Plugin for GamePlugin {
                 .with_length_unit(8.)
                 .with_collision_hooks::<OneWayPlatformHooks>(),
         );
-        app.add_plugins(PhysicsDebugPlugin);
+        // app.add_plugins(PhysicsDebugPlugin);
         // app.add_plugins(PhysicsDiagnosticsPlugin);
         app.add_plugins(CursorCoordsPlugin);
         app.add_plugins(LevelBgmPlugin);
